9778962304f5ed0595e8354aec1b04fce37a4330
Hǎiliàng Wáng <w@h12.me>
2017-07-26 15:27:12 +0800

    use noto font

--- cmd/webshotd/build.bash
+++ cmd/webshotd/build.bash
@@ -7,5 +7,5 @@ go install -tags gtk_3_18 github.com/gotk3/gotk3/gtk
 go build -v -tags gtk_3_18 -gcflags "-N -l"
 docker build . -t h12w/webshotd
 docker push h12w/webshotd
-# docker run --publish 9191:9191 --detach h12w/webshotd
+#docker run --name webshotd --publish 9191:9191 --detach h12w/webshotd
 fi
